bab3df99727fd93fd4a7cd16036c2a31e0a32bf3
Christophe de Dinechin <christophe@taodyne.com>
2012-08-07 18:59:28 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao

--- doc/movie_credits.doxy.h
+++ doc/movie_credits.doxy.h
@@ -14,7 +14,7 @@
  * @include movie_credits.ddd
  *
  * Here is a screen capture of this document:
- * @image html "movie_credits.png" "Output from movie_credits.ddd"
+ * @image html "movie_credits.png" "Movie credits"
  *
  * @endtaomoduledescription{MovieCredits}
  *
@@ -82,7 +82,7 @@ text mc_swc_clouds_texture;
  * after which the camera pans downwards to reveal a planet
  * (see @ref mc_swc_planet_texture).
  * The text to display is given in @p Body. It may contain any standard
- * text formatting primitive such as @ref align,  @ref font or @ref color,
+ * text formatting primitive such as @p align,  @p font or @p color,
  * but suitable initial values are provided if none is specified.
  * @~french
  * Affiche un texte défilant dans le style du générique d'ouverture de Star Wars.
@@ -90,8 +90,8 @@ text mc_swc_clouds_texture;
  * après quoi la caméra s'incline vers le bas pour révéler une planète
  * (cf. @ref mc_swc_planet_texture).
  * Le texte à afficher est donné dans @p Body. Il peut contenir n'importe
- * quelle primitive de formattage de texte, par exemple @ref align,  @ref font
- * ou @ref color. Les valeurs par défaut sont adaptées au style Star Wars.
+ * quelle primitive de formattage de texte, par exemple @p align,  @p font
+ * ou @p color. Les valeurs par défaut sont adaptées au style Star Wars.
  */
 mc_star_wars_crawl(real Duration, tree Body);
 
@@ -100,13 +100,13 @@ mc_star_wars_crawl(real Duration, tree Body);
  * Draw a ship designed to integrate in a Star Wars crawl.
  * The ship appears after @p Delay seconds, moves at a speed of @p Speed
  * units per second towards the background. The rendering of the ship
- * is made by @p Body, and can use any rendering primitive (e.g. @ref object).
+ * is made by @p Body, and can use any rendering primitive (e.g. @p object).
  * @~french
  * Affiche un vaisseau spatial sur l'écran Star Wars.
  * Le vaisseau apparaît après @p Delay secondes, se déplace à une vitesse de
  * @p Speed unités par seconde vers le fond de l'écran. L'affichage du vaisseau
  * est fait par @p Body, et peut utiliser n'importe quelle primitive (par
- * exemple, @ref object du module ObjectLoader).
+ * exemple, @p object du module ObjectLoader).
  */
 mc_star_wars_crawl_ship(real Delay, real Speed, tree Body);
 
@@ -147,12 +147,12 @@ mc_credit(text Role, text Name);
  * Draw a section, for example to announce the cast.
  * @param Height specifies the height of the box in which the section is shown.
  * @param Body specifies the contents of the section. It may include text
- * or graphics. The @p Body executes in a @ref text_box.
+ * or graphics. The @p Body executes in a @p text_box.
  * @~french
  * Affiche un nouveau bloc dans le générique déroulant.
  * @param Height est la hauteur du bloc.
  * @param Body est le contenu du bloc. Il peut s'agir de texte et/ou d'éléments
- * graphiques. La paramètre @p Body s'exécute dans une @ref text_box.
+ * graphiques. La paramètre @p Body s'exécute dans une @p text_box.
  */
 mc_credits_section(real Height, tree Body);
 
