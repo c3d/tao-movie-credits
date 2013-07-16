d9d7e8ac4a6a5382c0380dcff96c90adf7bd749e
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-07-16 15:36:41 +0200

    Do not draw depth attributes when we are in identify mode. Refs #3093.

--- doc/movie_credits.doxy.h
+++ doc/movie_credits.doxy.h
@@ -1,7 +1,8 @@
 /**
  * @~english
  * @taomoduledescription{MovieCredits, Movie Credits}
- * <tt>import MovieCredits</tt> - Movie-style rolling credits.@n
+ *
+ * <tt>import MovieCredits</tt> - Movie-style rolling credits
  *
  * MovieCredits is a collection of ready-to-use movie-style rolling credits.
  *
@@ -19,7 +20,8 @@
  *
  * @~french
  * @taomoduledescription{MovieCredits, Génériques de films}
- * <tt>import MovieCredits</tt> - Affichage de textes à la manière de génériques de films.@n
+ *
+ * <tt>import MovieCredits</tt> - Affichage de textes à la manière de génériques de films.
  *
  * Le module MovieCredits propose des commandes telles que
  * @ref mc_star_wars_crawl, qui permettent de créer des animations avec du
